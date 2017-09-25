6ce6579f7435c85388b56650c69bea9317b11d9a
Nicolas Seydoux <nseydoux@laas.fr>
2017-09-25 16:28:35 +0200

    Fixed om2m startup

--- project/run.sh
+++ project/run.sh
@@ -1,6 +1,7 @@
 echo "TECHIO> open --port 8080"
+cd /mooc/in
+sh start.sh
+cd /project
 mvn -q compile
-sh /mooc/in/start.sh
 curl localhost:8080
-sh in/start.sh
 mvn -q exec:java -Dconfig=config.json -Dexec.classpathScope=compile
