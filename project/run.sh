cdc6fc2b1e11e9a45af36d8612e99fc9841c33ae
Nicolas Seydoux <nseydoux@laas.fr>
2017-09-25 21:43:37 +0200

    Changed the path for compilation

--- project/run.sh
+++ project/run.sh
@@ -1,7 +1,7 @@
 echo "TECHIO> open --port 8080"
 cd /mooc/in
 sh start.sh &
-cd /project
+cd /project/target
 mvn -q compile
 curl localhost:8080
 mvn -q exec:java -Dconfig=config.json -Dexec.classpathScope=compile
