2be91a2e4ee3fcb46332e425b3c8e45ac5c89c28
Nicolas Seydoux <nseydoux@laas.fr>
2017-09-25 16:18:17 +0200

    Removed om2m from the project (too heavy)

--- project/in/start.sh
+++
@@ -1,1 +0,0 @@
-java -Xmx512M -jar -ea -Declipse.ignoreApp=true -Dosgi.clean=true -Ddebug=true plugins/org.eclipse.equinox.launcher_1.3.0.v20140415-2008.jar -console -noExit
--- project/run.sh
+++ project/run.sh
@@ -1,4 +1,7 @@
+
 mvn -q compile
+echo `ls /mooc`
+curl localhost:8080
 echo "TECHIO> open --port 8080"
 sh in/start.sh
 mvn -q exec:java -Dconfig=config.json -Dexec.classpathScope=compile
