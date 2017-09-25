b4831c68d5e346262796f8ce08b36ee5b09bdaaf
Nicolas Seydoux <nseydoux@laas.fr>
2017-09-25 16:24:18 +0200

    Forced om2m execution

--- project/run.sh
+++ project/run.sh
@@ -1,7 +1,6 @@
-
-mvn -q compile
-echo `ls /mooc`
-curl localhost:8080
 echo "TECHIO> open --port 8080"
+mvn -q compile
+sh /mooc/in/start.sh
+curl localhost:8080
 sh in/start.sh
 mvn -q exec:java -Dconfig=config.json -Dexec.classpathScope=compile
