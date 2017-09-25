353ab84f423f49f92eaba59335c354827445eeb9
Nicolas Seydoux <nseydoux@laas.fr>
2017-09-25 15:52:57 +0200

    Opened techio port for the om2m instance

--- project/run.sh
+++ project/run.sh
@@ -1,4 +1,5 @@
 mvn -q compile
+echo "TECHIO> open --port 8080"
 echo "TECHIO> open --port 9555"
 echo "TECHIO> open --port 9556"
 mvn -q exec:java -Dconfig=config.json -Dexec.classpathScope=compile
