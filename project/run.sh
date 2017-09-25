58f91de4a3bb393f82975c3ef6d452d441557ef7
Nicolas Seydoux <nseydoux@laas.fr>
2017-09-25 22:21:49 +0200

    Tested instantiating the server before the viewer

--- project/run.sh
+++ project/run.sh
@@ -1,7 +1,6 @@
-echo "TECHIO> open --port 8080 /project/viewer.html"
 cd /mooc/in
 sh start.sh &
 cd /project/target
 mvn -q compile
-curl localhost:8080
+echo "TECHIO> open --port 8080 /project/viewer.html"
 mvn -q exec:java -Dconfig=config.json -Dexec.classpathScope=compile
